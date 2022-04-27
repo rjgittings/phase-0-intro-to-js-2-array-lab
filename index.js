17c6a74dfa785ba4e4579231a22344467b086837
Robert Gittings <rjgitting@gmail.com>
2022-04-26 21:41:33 -0400

    commit tree baba2968836ff8d1114ef19185f269fdaa84e546

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat (john) {
+    return [...cats, john];
+  }
+  function prependCat (name) {
+    return [name, ...cats];
+  }
+  function removeFirstCat () {
+    return cats.slice(1);
+  }
+  function removeLastCat() {
+      return cats.slice(0, cats.length - 1)
+  }
+  
